f4c1106533ae62429da7fbc65eece03b8157cfab
idamarhare kevin <kidamarhare@gmail.com>
2024-11-19 15:28:58 +0100

    visitor homepage

--- app/controllers/island_controller.rb
+++ app/controllers/island_controller.rb
@@ -12,9 +12,24 @@ class IslandController < ApplicationController
     end
   end
 
+  def index
+    @islands = Island.all
+
+    @islands = @islands.where("capacity >= ?", params[:capacity]) if params[:capacity].present?
+    @islands = @islands.where(location: params[:location]) if params[:location].present?
+    @islands = @islands.where("price <= ?", params[:price]) if params[:price].present?
+
+    render :index
+  end
+
+  def show
+    @bookings = Booking.new
+    @reviews = Review.new
+  end
+
   private
 
   def list_params
     params.require(:list).permit(:name, :description, :location, :gps_longitude, :gps_latitude, :price_per_night; :capacity)
-      end
+  end
 end
--- app/models/island.rb
+++ app/models/island.rb
@@ -8,4 +8,5 @@ class Island < ApplicationRecord
   validates :capacity, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :gps_longitude, presence: true, format: { with: /\A[-+]?\d{1,3}\.\d+\z/, message: "must be a valid longitude" }
   validates :gps_latitude, presence: true, format: { with: /\A[-+]?\d{1,2}\.\d+\z/, message: "must be a valid latitude" }
+
 end
