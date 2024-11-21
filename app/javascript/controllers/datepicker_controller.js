9cbf89dab37452efe979a101432aedd780f7e2e0
guillaumeautret <guillaume.autret@gmail.com>
2024-11-21 15:03:37 +0100

    le prix varie en fonction des dates selectionnées

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -3,13 +3,14 @@ import flatpickr from "flatpickr";
 
 // Connects to data-controller="datepicker"
 export default class extends Controller {
-  static targets = ["dateRange", "startDate", "endDate"]
+  static targets = ["dateRange", "startDate", "endDate", "nights", "pricePerNight", "totalPrice"]
 
   connect() {
     this.flatpickr = flatpickr(this.dateRangeTarget, {
       mode: "range",
       minDate: "today",
       dateFormat: "Y-m-d",
+      // quand la valeur du champ date change, on met à jour les champs cachés
       onChange: (selectedDates) => {
         this.updateHiddenFields(selectedDates);
       }
@@ -25,6 +26,13 @@ export default class extends Controller {
     }
     if (endDate) {
       this.endDateTarget.value = this.flatpickr.formatDate(endDate, "Y-m-d");
+      // Quand on a la date de fin, on peut calculer le nombre de nuits, et le prix
+      const nights = Math.round((endDate - startDate) / (1000 * 60 * 60 * 24));
+      this.nightsTarget.textContent = nights;
+      this.totalPriceTarget.textContent = nights * this.pricePerNightTarget.innerText;
+    } else {
+      // Si endDate n'est pas encore sélectionnée, on remet le nombre de nuits à 0
+      this.nightsTarget.textContent = "0";
     }
   }
 
